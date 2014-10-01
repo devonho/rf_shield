7bcf08003361ab54a7d055fca81251ee3991ed97
devonho <devonho@msn.com>
2014-10-01 19:34:14 +0800

    Working with RF RX.

--- rf_shield/rf_shield.ino
+++ rf_shield/rf_shield.ino
@@ -28,17 +28,16 @@
  * room for 256 samples.
  */
 volatile unsigned short timestamps[256];
+volatile unsigned short syncstamp;
 volatile unsigned short i = 0;
 volatile bool finished = false; // Set to true when sampling phase finished
+volatile bool syncfound = false; // Set to true when sampling phase finished
 const unsigned short timeout_msecs = 5000; // Upper bound on sampling phase
 
 void setup(void)
 {
 	Serial.begin(115200);
-	Serial.println(F("Recording input change timestamps. Will stop after"));
-	Serial.print(F(" 255 changes, or "));
-	Serial.print(timeout_msecs, DEC);
-	Serial.println(F(" msecs, whichever occurs first."));
+	Serial.println(F("=== Start ==="));
 	Serial.flush();
 
 	cli(); // Disable interrupts while setting up
@@ -59,7 +58,7 @@ void setup(void)
 	//     - ICES1 = 1 (rising edge detect)
 	//     - ICIE1 = 1 (enable interrupt on input capture)
 	TCCR1A = B00000000; // COM1A1 COM1A0 COM1B1 COM1B0 - - WGM11 WGM10
-	TCCR1B = B01000001; // ICNC1 ICES1 - WGM13 WGM12 [CS12 CS11 CS10] - Clock Select = CLKio/1 (No prescaling)
+	TCCR1B = B00000010; // ICNC1 ICES1 - WGM13 WGM12 [CS12 CS11 CS10] - Trigger on falling edge, Clock Select = CLKio/8 
 	TCCR1C = B00000000; // FOC1A FOC1B - - - - - -
 	TIMSK1 = B00100000; // - - ICIE1 - - OCIE1B OCIE1A TOIE1, overflow interrupt disabled.
 
@@ -73,79 +72,148 @@ void setup(void)
 	sei(); // Re-enable interrupts
 }
 
+void enableCapture(boolean en)
+{
+  cli(); // Disable interrupts while setting up  
+  if(en)
+  {
+    //  - Reset Timer/Counter1 and Input Capture Register 1
+    TIMSK1 = B00100000; // Enable interrupts    
+  }
+  else
+  {
+    TIMSK1 = B00000000; // Disable interrupts
+  }
+  TCNT1 = 0x0000;
+  ICR1 = 0x0000;  
+  TIFR1 = B00100001; // Kill pending interrupts (1 clears)  
+  sei(); // Re-enable interrupts
+}
+
 // Timer1 Capture Event interrupt handler; triggers when ICF1 is set
 // Measuring TCNT1 at the start and end of this suggests runtime == 52 cycles
 ISR(TIMER1_CAPT_vect)
 {
 	// Store ICR1 register into timestamps array
 	unsigned short t = ICR1;
-	// Change trigger edge
-        if(TCCR1B & _BV(ICES1))
+
+        if(!syncfound)
         {
+          if(TCCR1B & _BV(ICES1))
+          {
             // Rising edge detected
-            TCNT1 = 0x0000;
-	    ICR1 = 0x0000;
+            syncstamp = t;
+            if(syncstamp > 20000)
+            {
+              syncfound = true;
+              i=0;
+            }
+          }
+          TCNT1 = 0x0000;
+          ICR1 = 0x0000;
+          TCCR1B ^= _BV(ICES1); // Toggle edge to trigger on
         }
         else
         {
+  	  // Change trigger edge
+          if(TCCR1B & _BV(ICES1))
+          {
+            // Rising edge detected
+            TCNT1 = 0x0000;
+  	    ICR1 = 0x0000;
+          }
+          else
+          {
             // Falling edge detected
             timestamps[i++] = t;
+          }
+          TCCR1B ^= _BV(ICES1); // Toggle edge to trigger on
+  
+  	  if (i == 24) 
+          {
+            TIMSK1 = B00000000; // Disable interrupts
+            TCNT1 = 0x0000;
+            ICR1 = 0x0000;  
+            TIFR1 = B00100001; // Kill pending interrupts (1 clears)  
+            finished = true; // Signal completion
+            i=0;
+            syncfound=false;
+            return;
+  	  }
         }
-        TCCR1B ^= _BV(ICES1);
-
-	// The timestamp value 0 is reserved for Timer1 overflows
-	//if (t == 0)
-	//	t--; // Wrap back by one clock cycle (1/16 usec)
-	//timestamps[i] = t;
-
-	if (i == 255) {
-		// Disable Input capture interrupt, and end sample collection
-		TIMSK1 = B00000000; // Disable interrupts
-		TIFR1 = B00100001; // Kill pending interrupts (1 clears)
-		finished = true; // Signal completion
-		return;
-	}
-}
-
-// Timer1 Overflow interrupt handler; triggers when TOV1 is set
-// Measuring TCNT1 at the start and end of this suggests runtime == 42 cycles
-ISR(TIMER1_OVF_vect)
-{
-	// Store 0 value into timestamps array
-	timestamps[i] = 0;
-
-	if (i == 255 || finished) {
-		// Disable Input capture interrupt, and end sample collection
-		TIMSK1 = B00000000; // Disable interrupts
-		TIFR1 = B00100001; // Kill pending interrupts (1 clears)
-		finished = true; // Signal completion
-		return;
-	}
-	i++;
 }
 
 void loop(void)
 {
-	digitalWrite(12, HIGH);   // turn the LED on (HIGH is the voltage level)
+  //continuousTriggerAndCapTest();
+  //singleTriggerAndCapTest();
+  rfCapTest();
+}
+
+void rfCapTest(void)
+{
+  unsigned long pingpong[2];
+  unsigned char k;
+  while(true)
+  {    
+    if(finished)
+    {
+      k=++k%2;
+      pingpong[k]=0;
+      for(int j=0;j<24;j++)
+      {
+        pingpong[k] += (timestamps[j]>1000?1:0)<<j;
+      }
+      if(pingpong[0]==pingpong[1])
+        Serial.println(pingpong[k],HEX);
+      finished = false;
+      enableCapture(true);     
+    }  
+  }
+
+}
+
+void continuousTriggerAndCapTest(void)
+{
+  while(true)
+  {    
+    digitalWrite(12, HIGH);   // Trigger 1527
+    if(finished)
+    {
+      unsigned char bits[24];
+      for(int j=0;j<24;j++)
+      {
+        bits[j]=timestamps[j]>1000?1:0;
+      }
+      Serial.print(F("Bits: "));
+      for(int j=0;j<24;j++)
+      {      
+        Serial.print(bits[j]);
+      }
+      Serial.println(F(" "));
+
+      finished = false;
+
+      enableCapture(true);     
+      
+      digitalWrite(12, LOW);   // Trigger 1527
+      digitalWrite(12, HIGH);   // Trigger 1527
+    }  
+  }
+}
+
+void singleTriggerAndCapTest(void)
+{
+	digitalWrite(12, HIGH);   // Trigger 1527
 	delay(88);
-	digitalWrite(12, LOW);    // turn the LED off by making the voltage LOW
-
-	digitalWrite(13, HIGH);    // turn the LED off by making the voltage LOW
-	delay(100);              // wait for a second
-	digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW  
-
-	// Spin until either 'finished' is set, or timeout_msecs have elapsed
-	//unsigned long t = millis();
-	//while (!finished && millis() - t < timeout_msecs);
+	digitalWrite(12, LOW);    
 
 	// Broadcast the received samples over the serial port
-	Serial.print(F("Recorded "));
-	Serial.print(i, DEC);
-	Serial.println(F(" input changes. Timestamps:"));
+	Serial.println(F("Timestamps:"));
 	Serial.println(F("--BEGIN--"));
-	for (unsigned char j = 0; j < i; j++) {
-		//Serial.print(j, DEC);
-		//Serial.print(":");
+        Serial.println(syncstamp, DEC);
+        Serial.println(F("---"));
+	for (unsigned char j = 0; j < 24; j++) {
 		Serial.println(timestamps[j], DEC);
 	}
 	Serial.println(F("--END--"));
